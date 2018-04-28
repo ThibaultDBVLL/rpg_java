3aa39c3301ed65d15d89080d281f7bb1e458ba32
ThibaultDBVLL <thibault.daboville@gmail.com>
2018-04-28 17:28:45 +0200

    code works until the first if (line 16)

--- rpg.java
+++ rpg.java
@@ -4,18 +4,16 @@ class rpg{
         String jouer;
         //String oui = "oui";
         //String non = "non";
-        //Boolean wePlay = false;
         Scanner rep_user = new Scanner(System.in);
 
         System.out.println("Tu veux jouer à un super rpg ? (Ecris 'Oui' ou 'non')");
-        jouer = rep_user.nextLine();
+        jouer = rep_user.next();
         System.out.println(jouer);
         rep_user.close();
-        //wePlay = rep_user.equals(oui);
         
-        if (jouer == "oui") {
+        if (jouer.equals("oui")) {
             System.out.println("trop cool !");
-        }
+        } 
         
     }
 }
